13417158533e40f0834225f5191d44c17a391415
helcio <h.andre@giap.pl>
2023-08-02 12:50:25 +0100

    Add alt

--- src/components/GameTitle.tsx
+++ src/components/GameTitle.tsx
@@ -1,7 +1,7 @@
 function GameTitle() {
   return (
     <header className='app-header'>
-      <img src='src/assets/ux.png' alt='' />
+      <img src='src/assets/ux.png' alt='an image representing ux/ui' />
       <h1>Frontend Quiz</h1>
     </header>
   );
