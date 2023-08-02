ff9202e4596480d52490eb489b9bd9a4f5aac1c3
helcio <h.andre@giap.pl>
2023-08-02 12:57:07 +0100

    change game title image file directory

--- src/components/GameTitle.tsx
+++ src/components/GameTitle.tsx
@@ -1,7 +1,7 @@
 function GameTitle() {
   return (
     <header className='app-header'>
-      <img src='src/assets/ux.png' alt='an image representing ux/ui' />
+      <img src='/ux.png' alt='an image representing ux/ui' />
       <h1>Frontend Quiz</h1>
     </header>
   );
